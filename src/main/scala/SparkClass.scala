daa25b03c9132f719064940f5da0e9061a7dba72
slyroman <72763566+slyroman@users.noreply.github.com>
2020-10-13 13:58:44 +0500

    last

--- src/main/scala/SparkClass.scala
+++ src/main/scala/SparkClass.scala
@@ -3,16 +3,14 @@ import org.apache.spark.sql._
 
 object SparkClass extends App {
 
-  val sorce_file = args(0)
-  val sorce_file_dict = args(1)
-  val result_file = args(2)
-
+    val sorce_file = args(0)
+    val sorce_file_dict = args(1)
+    val result_file = args(2)
 
     import org.apache.spark.sql.SparkSession
 
-
     val spark = {
-      SparkSession.builder()
+    SparkSession.builder()
         //.config("spark.sql.autoBroadcastJoinThreshold", 0)
         //.config("spark.driver.memory", "271859200")
         .master("local[*]")
@@ -29,9 +27,6 @@ object SparkClass extends App {
       .option("inferSchema", "true")
       .csv(sorce_file)
 
-
-
-
     val crimeFacts_ = crimeFacts.na.fill("Other", Seq("DISTRICT"))
 
     //crimeFacts_.describe("DISTRICT").show
@@ -47,15 +42,12 @@ object SparkClass extends App {
 
     var t1 = crimeFacts_
       .groupBy($"DISTRICT", $"YEAR", $"MONTH")
-
       .agg(
         count($"INCIDENT_NUMBER").as("crimes_total"),
-
         avg($"Lat").as("lat"),
         avg($"Long").as("lng")
       )
       .orderBy($"crimes_total".desc)
-
       .groupBy($"DISTRICT")
       .agg(
         sum($"crimes_total").as("crimes_total"),
@@ -63,9 +55,7 @@ object SparkClass extends App {
         avg($"lat").as("lat"),
         avg($"lng").as("lng")
       )
-
     //t1.show(false)
-
 
     import org.apache.spark.sql.expressions.Window
     import org.apache.spark.sql.functions.broadcast
@@ -75,29 +65,23 @@ object SparkClass extends App {
     // Window definition
     val w = Window.partitionBy($"DISTRICT").orderBy(desc("count"))
 
-
     var t2 = crimeFacts_
       .join(offenseCodesBroadcast, $"CODE" === $"OFFENSE_CODE")
       .withColumn("NAME_GROUP", split(col("NAME"), "\\-").getItem(0))
-
       .groupBy($"DISTRICT", $"NAME_GROUP")
       .agg(
         count("INCIDENT_NUMBER").as("count")
       )
       .orderBy($"DISTRICT", $"count".desc)
-
-
       //groupBy($"DISTRICT")
-
-      // Filter
+      // Filter by rank
       .withColumn("rank", rank.over(w)).where($"rank" <= 3)
-
       .groupBy('DISTRICT).agg(concat_ws(", ", collect_set($"NAME_GROUP")).as("frequent_crime_types")
     )
-
-
     //t2.show(100,false)
 
+
+    // final join
     val final_df = t1
       .join(t2, t1("DISTRICT") === t2("DISTRICT"))
       .withColumn("frequent_crime_types", regexp_replace($"frequent_crime_types", " ,", ",")).as("frequent_crime_types")
@@ -112,7 +96,5 @@ object SparkClass extends App {
 
     //final_df.write.mode(SaveMode.Overwrite).format("parquet").saveAsTable("boston_crimes_final.parquet")
     final_df.coalesce(1).write.mode("overwrite").parquet(result_file)
-
-
 
 }
